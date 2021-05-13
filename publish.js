7cb27cc764f4ce0b1692ba70c42133f84b7bbfd0
xinxian_mu <xinxian_mu@yunzhijia.com>
2021-05-13 12:27:28 +0800

    chore: 增加版本发布脚本

---
+++ publish.js
@@ -0,0 +1,16 @@
+var process = require('child_process');
+
+// 发布补丁
+// npm version patch
+
+// 发布小版本
+// npm version minor
+
+// 发布大版本
+// npm version major
+process.exec('npm version patch && npm publish',function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+    console.log(stdout)
+});
