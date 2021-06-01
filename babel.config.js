08dceb95a01bbb5797f9a96c0f16179532e72144
xinxian_mu <xinxian_mu@yunzhijia.com>
2021-06-01 12:18:57 +0800

    feat: 优化babel打包

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
 module.exports = {
     "presets": [
-        ["@babel/preset-env", {
-            "targets": "> 0.25%, not dead",
-            "useBuiltIns": "usage",
-            "debug": false,
-            "corejs": 3 // 指定 runtime-corejs 的版本，目前有 2 3 两个版本
-        }]
-    ],
-    "plugins": [
-        [
-        "@babel/plugin-transform-runtime"
-        ]
+        ["@babel/preset-env"]
     ]
 }
